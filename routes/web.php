6185cac20db08f6c428a53aa892043c54f80daf8
onur-akdogan <onurakdogan7106@gmail.com>
2022-02-24 10:57:28 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use App\Http\Controllers\MobilAppController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\UserController;
 use App\Models\AuthorsPost;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\config\Lfm;
 use \App\Http\Controllers\Backend\AdController;
@@ -31,7 +30,6 @@ use App\Http\Controllers\Backend\CornerPostsController;
 use App\Http\Controllers\Frontend\SitemapController;
 use App\Http\Controllers\ThemeController;
 use \App\Models\Post;
-use Spatie\Analytics\Period;
 
 
 /*
@@ -86,66 +84,7 @@ Route::get('/startbot', function () {
 
 // ADMİN Routes
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
-
-    Route::get('/dashboard', function () {
-        $news=DB::table('posts')->get('id');
-//        $endNews=Post::limit(10)->latest()->get();
-//        $endNewsCount=Post::orderByUniqueViews()->latest()->get();
-        $endComments=DB::table('comments')->limit(10)->latest('id')->get();
-        $endAuthors_posts=AuthorsPost::leftjoin('authors', 'authors.id', '=', 'authors_posts.authors_id')
-            ->select(['authors_posts.*', 'authors.name'])
-            ->latest()
-            ->paginate(10);
-        $newsCount=count($news);
-        $comments=DB::table('comments')->get('id');
-        $commentsCount=$comments->count();
-        $authors_posts=DB::table('authors_posts')->get('id');
-
-//        $analyticsData=Analytics::fetchMostVisitedPages(Period::days(7));
-        $endNews=Analytics::fetchMostVisitedPages(Period::days(1)); //son 24 saatte ençok ziyaret edilen sayfalar
-        $topReferrers=Analytics::fetchTopReferrers(Period::days(1)); //Yönlendirme yapan yerler ve sayfa gösterimleri
-        $userTypes =Analytics::fetchUserTypes(Period::days(1)); //Geri dönen ve Yeni gelen ziyaretçiler
-        $userTypesWeek =Analytics::fetchUserTypes(Period::days(7)); //Geri dönen ve Yeni gelen ziyaretçiler
-
-        $analyticsData = Analytics::performQuery(
-            Period::days(1),
-            'ga:sessions',
-            [
-                'metrics' => 'ga:sessions, ga:pageviews',
-                'dimensions' => 'ga:yearMonth'
-            ]
-        ); // günlük toplam oturum ve sayfa görüntüleme oranı
-        $analyticsDataMonth = Analytics::performQuery(
-            Period::days(30),
-            'ga:sessions',
-            [
-                'metrics' => 'ga:users, ga:pageviews',
-                'dimensions' => 'ga:yearMonth'
-            ]
-        ); // günlük toplam oturum ve sayfa görüntüleme oranı
-        $mostVisitedPages =Analytics::fetchMostVisitedPages(Period::days(1)); //çok görüntülenen haberler
-//        $analyticsDataDays = Analytics::performQuery(
-//            Period::days(1),
-//            'ga:sessions',
-//            [
-//                'metrics' => 'ga:sessions, ga:pageviews',
-//                'dimensions' => 'ga:yearMonth'
-//            ]
-//        );
-//        echo $endNews[0]['pageTitle'];
-//        dd($topReferrers);
-
-//        echo $analyticsData['totalsForAllResults']['ga:sessions'];
-//        dd($analyticsData);
-//        $peryot=Views::forViewable(Post::class)->countAndRemember();
-
-//        dd($newVisitor);
-        $authors_postsCount=$authors_posts->count();
-        return view('admin.index',compact('newsCount','commentsCount','endNews','endComments','endAuthors_posts','authors_postsCount','analyticsData','analyticsDataMonth','topReferrers','userTypes','userTypesWeek'));
-
-    });
-
-
+    Route::get('/dashboard',[AdminController::class,'index'])->name('dashboard');
 
     Route::get('/DBTrans',[ExtraController::class,'DBTrans']);
 
@@ -480,5 +419,7 @@ Route::get('/Category/{slug}/{id}', [ExtraController::class, 'CategoryPost']);
 Route::get('/{id}/', [ExtraController::class, 'GetDistrict']);
 
 //Route::get('/', [ExtraController::class, 'akbankkur']);
+
+
 
 
